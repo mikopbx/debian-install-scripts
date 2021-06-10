ab9d74bead6c28ba93bb930d90e0806e4b82ef90
apor <apor@miko.ru>
2021-06-10 18:06:06 +0300

    Добавил скрипты сборки phalcon

---
+++ packages/10-php-zephir-parser.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. "${ROOT_DIR}/libs/functions.sh";
+
+LIB_VERSION='1.3.6';
+LIB_URL="https://github.com/zephir-lang/php-zephir-parser/archive/refs/tags/v${LIB_VERSION}.tar.gz";
+LIB_PRIORITY='20';
+LIB_PHP_MODULE_PREFIX_INI='';
+LIB_CONFIGURE_OPTIONS='';
+LIB_NAME='zephir_parser';
+
+installPhpExtension "$LIB_NAME" "$LIB_URL" "$LIB_PRIORITY" "$LIB_PHP_MODULE_PREFIX_INI" "$LIB_CONFIGURE_OPTIONS"
---
+++ packages/30-php-phalcon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. "${ROOT_DIR}/libs/functions.sh";
+
+LIB_VERSION='4.1.0';
+LIB_URL="https://github.com/phalcon/cphalcon/archive/v${LIB_VERSION}.tar.gz";
+LIB_PRIORITY='40';
+LIB_PHP_MODULE_PREFIX_INI='';
+LIB_NAME='phalcon';
+
+curl -L 'https://github.com/zephir-lang/zephir/releases/download/0.12.21/zephir.phar' -o zephir;
+chmod +x zephir;
+srcDirName=$(downloadFile "$LIB_URL");
+(
+  cd "$srcDirName" || exit;
+  {
+    ../zephir fullclean;
+    ../zephir build;
+    cp ./ext/modules/phalcon.so "$(php-config --extension-dir)/";
+  } >> "$LOG_FILE" 2>> "$LOG_FILE";
+)
+
+rm -rf "$srcDirName" ./zephir;
+enablePhpExtension "$LIB_NAME" "$LIB_PRIORITY" "$LIB_PHP_MODULE_PREFIX_INI"

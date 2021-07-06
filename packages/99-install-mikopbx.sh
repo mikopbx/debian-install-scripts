cb3cab7e8a8391944b3294eeec3abd89ed54cc1d
apor <apor@miko.ru>
2021-07-06 14:18:04 +0300

    Добавил отключение сервиса rsyslog.

--- packages/99-install-mikopbx.sh
+++ packages/99-install-mikopbx.sh
@@ -55,4 +55,6 @@ ln -s /etc/rc/debian/mikopbx_lan_dhcp \
       /etc/dhcp/dhclient-enter-hooks.d/mikopbx_lan_dhcp
 ln -s /usr/www/resources/rootfs/usr/lib64/extensions/no-debug-zts-20190902/mikopbx.so \
       /usr/lib64/extensions/no-debug-zts-20190902/mikopbx.so
+
+systemctl disable rsyslog;
 )
